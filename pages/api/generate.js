0332432f0329d963ec0380ac3a02a457810404d4
Mohammed Rafique <mohammedrafique23@gmail.com>
2023-02-09 22:18:37 +0530

    Fetching multiple combination records

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -10,49 +10,75 @@ export default async function (req, res) {
   if (!configuration.apiKey) {
     res.status(500).json({
       error: {
-        message: "OpenAI API key not configured, please follow instructions in README.md",
-      }
+        message:
+          "OpenAI API key not configured, please follow instructions in README.md",
+      },
     });
     return;
   }
 
-  const Businesskeyword = req.body.Businesskeyword || '';
-  const clientKeyword=req.body.clientKeyword || '';
+  const Businesskeyword = req.body.Businesskeyword || "";
+  const clientKeyword = req.body.clientKeyword || "";
   if (Businesskeyword.trim().length === 0 && clientKeyword.trim().length) {
     res.status(400).json({
       error: {
         message: "Please enter a valid keyword",
-      }
+      },
     });
     return;
   }
 
   try {
-    
-    const promtReplace=PROMPT.replace("<Business Description>",Businesskeyword)
-    const ReplacementPromt=promtReplace.replace("<Client Description>",clientKeyword)
-    const response = await openai.createCompletion({
-      model:"text-davinci-003",
-      prompt: ReplacementPromt,
-      temperature: 0.7,
-      max_tokens: 400,
-      top_p: 1,
-      frequency_penalty: 0,
-      presence_penalty: 0,
-    });
-    // console.log("question", ReplacementPromt, response.data + '<br>' + imageElement)
-    res.status(200).json({ result: response.data.choices[0].text });
-  } catch(error) {
+    let businessKeywordsArray = [];
+    let clientKeywordsArray = [];
+    businessKeywordsArray = Businesskeyword.split(",");
+    clientKeywordsArray = clientKeyword.split(",");
+    if (businessKeywordsArray.length !== clientKeywordsArray.length) {
+      throw new Error(
+        "Business keyword length does not match with client keyword length"
+      );
+    }
+    let response;
+    let responseData = [];
+    for (let i = 0; i <= businessKeywordsArray.length - 1; i++) {
+      const promtReplace = PROMPT.replace(
+        "<Business Description>",
+        businessKeywordsArray[i]
+      );
+      const ReplacementPromt = promtReplace.replace(
+        "<Client Description>",
+        clientKeywordsArray[i]
+      );
+      response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: ReplacementPromt,
+        temperature: 0.7,
+        max_tokens: 400,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+      });
+      responseData.push(response.data.choices[0].text);
+    }
+    console.log("this is response data", responseData);
+    res.status(200).json({ result: responseData });
+  } catch (error) {
     // Consider adjusting the error handling logic for your use case
     if (error.response) {
-      console.error(error.response.status, error.response.data);
-      res.status(error.response.status).json(error.response.data);
+      console.error(
+        error.response.status,
+        error.response.data,
+        error.response.statusText
+      );
+      res
+        .status(error.response.status)
+        .json(error.response.data, error.response.statusText);
     } else {
       console.error(`Error with OpenAI API request: ${error.message}`);
       res.status(500).json({
         error: {
-          message: 'An error occurred during your request.',
-        }
+          message: "An error occurred during your request.",
+        },
       });
     }
   }
